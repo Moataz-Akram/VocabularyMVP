9bc5408b79c0514863c3293627a2344664d61c0e
moataz-akram <moatazakram0@gmail.com>
2026-07-16 01:52:03 +0300

    Initial Commit

---
+++ VocabularyMVP/VocabularyMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyMVPApp.swift
+//  VocabularyMVP
+//
+//  Created by Moataz Akram on 16/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
